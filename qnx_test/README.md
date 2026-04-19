1332f0d4f0ca6f7723d1435f9d014484e9e347dc
Walter <walt0005@e.ntu.edu.sg>
2026-04-19 16:18:49 +0800

    Create test folder with relevant files; amended all todo in mainc; added timer_create to integrate accurately into qnx

---
+++ qnx_test/README.md
@@ -0,0 +1,51 @@
+# MA4830 Waveform Generator - QNX Build
+## Null Terminators
+
+### Compile
+```
+cc -Wall -o wavegen main.c hw.c sine_wave_generator_3.c ui_graphics.c setup_input.c -lm
+```
+
+### Run
+```
+./wavegen                              (default: sine, 440Hz)
+./wavegen square 200 0.8 0.1           (square wave, 200Hz, amp 0.8, offset 0.1)
+./wavegen arb 100 1.0 0.0 wave1.txt   (arbitrary from file)
+```
+
+### Controls
+```
+Arrow Up/Down     Frequency (+/- 10%)
+Arrow Left/Right  Cycle waveform type
++/-               Amplitude (+/- 0.05)
+[/]               Offset (+/- 0.05)
+1-5               Direct waveform select (1=Sine 2=Square 3=Tri 4=Saw 5=Arb)
+w                 Cycle arbitrary wave file (wave.txt -> wave1.txt -> wave2.txt)
+f                 Type frequency value
+a                 Type amplitude value
+o                 Type offset value
+s                 Save settings to settings.dat
+l                 Load settings from settings.dat
+q                 Quit
+Ctrl+C            Quit (SIGINT)
+```
+
+### Expected Output
+- Oscilloscope: waveform on DAC channel 0
+- Terminal: ASCII dashboard with live wave preview, frequency, amplitude, offset
+- Potentiometer 0: controls amplitude (0.0-1.0)
+
+### Files Required
+```
+main.c                    - Threading, mutex, keyboard loop (Walter)
+hw.c / hw.h               - PCI DAC/ADC hardware layer (Qihong)
+sine_wave_generator_3.c   - Waveform math (Trudy/Misha)
+sine_wave.h               - Waveform header
+ui_graphics.c / .h        - ASCII display (Jaz)
+setup_input.c / .h        - Config parse, keyboard input (Alicia)
+wave.txt / wave1.txt / wave2.txt / wave3.txt  - Arbitrary waveform data
+```
+
+### Left TODO
+- analog switch reading for wave type cycling (DIO)
+- hardware graceful termination via switch
---
+++ qnx_test/src/hw.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include "hw.h"
+
+#ifdef __QNX__
+/* ---- REAL QNX HARDWARE CODE (Runs in lab) ---- */
+#include <hw/inout.h>
+#include <sys/mman.h>
+
+int hw_open(Device *d) {
+    struct pci_dev_info info;
+    int i;
+    memset(&info, 0, sizeof(info));
+    if (pci_attach(0) < 0) return -1;
+    info.VendorId = 0x1307; info.DeviceId = 0x01;
+    d->hdl = pci_attach_device(0, PCI_SHARE|PCI_INIT_ALL, 0, &info);
+    if (!d->hdl) return -1;
+    for (i = 0; i < 5; i++) 
+        d->iobase[i] = mmap_device_io(0x0f, PCI_IO_ADDR(info.CpuBaseAddress[i]));
+    return ThreadCtl(_NTO_TCTL_IO, 0);
+}
+
+void hw_dac(Device *d, int chan, unsigned short val) {
+    out16(d->iobase[1] + 8, (chan == 0) ? 0x0a23 : 0x0a43);
+    out16(d->iobase[4] + 2, 0);
+    out16(d->iobase[4] + 0, val);
+}
+
+void hw_close(Device *d) {
+    hw_dac(d, 0, 0x7FFF); /* Reset to mid-range */
+    pci_detach_device(d->hdl);
+}
+
+unsigned short read_adc(Device *d, unsigned short channel) {
+    unsigned short adc_val;
+    unsigned short chan_config;
+    chan_config = 0x0D00 | ((channel & 0x0F) << 4) | (channel & 0x0F);
+    out16(d->iobase[1] + 2, chan_config);
+    out16(d->iobase[2] + 2, 0);
+    delay(1);
+    out16(d->iobase[2] + 0, 0);
+    while(!(in16(d->iobase[1] + 2) & 0x4000));
+    adc_val = in16(d->iobase[2] + 0);
+    return adc_val;
+}
+
+void reset_dac(Device *d) {
+    out16(d->iobase[1] + 8, 0x0a23);
+    out16(d->iobase[4] + 2, 0);
+    out16(d->iobase[4] + 0, 0x8FFF);
+    out16(d->iobase[1] + 8, 0x0a43);
+    out16(d->iobase[4] + 2, 0);
+    out16(d->iobase[4] + 0, 0x8FFF);
+}
+#else
+/* ---- MOCK HARDWARE CODE (Runs on Windows/Linux locally) ---- */
+
+int hw_open(Device *d) {
+    (void)d;
+    printf("[MOCK] Hardware opened successfully. Bypassing PCI checks.\n");
+    return 0;
+}
+
+void hw_dac(Device *d, int chan, unsigned short val) {
+    static int count = 0;
+    (void)d; (void)chan;
+    /* Print every 307th sample (prime number avoids aliasing with 100-sample cycle) */
+    /* if (count++ % 307 == 0)
+        printf("[MOCK DAC] #%d val=%u\n", count, val); */
+}
+
+void hw_close(Device *d) {
+    (void)d;
+    printf("\n[MOCK] Hardware closed.\n");
+}
+
+#endif
